9f7d6c4d163c0c3c4a7da3a0c76fe09eb63d851c
AdamB25 <barsialtunit@gmail.com>
2023-02-02 04:37:51 +0300

    week 2 TC001 second test is done

--- src/test/java/com/cydeo/tests/week02/TC001.java
+++ src/test/java/com/cydeo/tests/week02/TC001.java
@@ -2,7 +2,9 @@ package com.cydeo.tests.week02;
 
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,21 +14,21 @@ import java.time.Duration;
 
 public class TC001 {
     WebDriver driver;
-    /*
-    1. Launch browser
-    2. Navigate to url 'http://automationexercise.com'
-    3. Verify that home page is visible successfully
-     */
+
     @BeforeMethod
     public void setUp() {
-         driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
         driver.get("http://automationexercise.com");
 
     }
-
+  /*
+    1. Launch browser
+    2. Navigate to url 'http://automationexercise.com'
+    3. Verify that home page is visible successfully
+     */
 
     @Test
     public void verifyHomePage() {
@@ -36,7 +38,26 @@ public class TC001 {
         Assert.assertEquals(actualTitle,expectedTitle);
 
        */
-        BrowserUtils.verifyTitle(driver,"Automation Exercise");
+        BrowserUtils.verifyTitle(driver, "Automation Exercise");
+
+    }
+
+    /*
+    4. Click on 'Signup / Login' button
+    5. Verify 'New User Signup!' is visible
+     */
+
+
+    @Test
+    public void verifyNewUserSignUpLabel() {
+        BrowserUtils.sleep(1);
+        clickModule("Signup / Login");
+        BrowserUtils.sleep(2);
+        WebElement labelForSignUp = driver.findElement(By.xpath("//h2[.='New User Signup!']"));
+
+        Assert.assertTrue(labelForSignUp.isDisplayed());
+        //option + Enter for creating method (mac)
+        //alt+enter (windows)
 
     }
 
@@ -44,5 +65,12 @@ public class TC001 {
     @AfterMethod
     public void tearDown() {
         driver.close();
+    }
+
+
+    private void clickModule(String moduleName) {
+        String locator = "//a[contains(.,'" + moduleName + "')]";
+        driver.findElement(By.xpath(locator)).click();
+
     }
 }
