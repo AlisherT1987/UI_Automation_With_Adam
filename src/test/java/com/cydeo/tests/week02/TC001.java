8995e2ca1eb769e5e7fe180d4ac9ea382fc8e440
AdamB25 <barsialtunit@gmail.com>
2023-02-02 03:54:12 +0300

    week 2 TC001 first test is done

--- src/test/java/com/cydeo/tests/week02/TC001.java
+++ src/test/java/com/cydeo/tests/week02/TC001.java
@@ -1,8 +1,48 @@
 package com.cydeo.tests.week02;
 
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class TC001 {
+    WebDriver driver;
+    /*
+    1. Launch browser
+    2. Navigate to url 'http://automationexercise.com'
+    3. Verify that home page is visible successfully
+     */
+    @BeforeMethod
+    public void setUp() {
+         driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        driver.get("http://automationexercise.com");
+
+    }
 
 
+    @Test
+    public void verifyHomePage() {
+      /* String expectedTitle="Automation Exercise";
+        String actualTitle = driver.getTitle();
+                               //actual     expected
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+       */
+        BrowserUtils.verifyTitle(driver,"Automation Exercise");
+
+    }
 
 
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
 }
