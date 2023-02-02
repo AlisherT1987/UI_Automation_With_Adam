73bd56e93d9e1d5500fc8438ef10b74aa8a027d9
AdamB25 <barsialtunit@gmail.com>
2023-02-02 06:03:11 +0300

    week 2 TC001 fourth test is started

--- src/test/java/com/cydeo/tests/week02/TC001.java
+++ src/test/java/com/cydeo/tests/week02/TC001.java
@@ -6,6 +6,7 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -72,6 +73,88 @@ public class TC001 {
 
     @Test
     public void verifyAccountInfo() {
+        signUp();
+
+        WebElement label = driver.findElement(By.xpath("//h2[contains(.,'Enter Account Information')]"));
+
+        Assert.assertTrue(label.isDisplayed());
+
+
+    }
+/*
+9. Fill details: Title, Name, Email, Password, Date of birth
+10. Select checkbox 'Sign up for our newsletter!'
+11. Select checkbox 'Receive special offers from our partners!'
+12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+13. Click 'Create Account button'
+14. Verify that 'ACCOUNT CREATED!' is visible
+15. Click 'Continue' button
+16. Verify that 'Logged in as username' is visible
+ */
+
+    @Test
+    public void verifyAccountCreation() {
+        signUp();
+
+        //click mr
+        driver.findElement(By.xpath("(//input[@type='radio'])[1]")).click(); //MR
+        // driver.findElement(By.xpath("(//input[@type='radio'])[2]")).click(); ///MRS
+        //enter passowrd
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Test12345");
+        //Select day
+        WebElement selectDayElement = driver.findElement(By.xpath("//select[@id='days']"));
+
+        Select select=new Select(selectDayElement);
+        select.selectByVisibleText("1");
+
+        //Select month
+        WebElement selectMonthElement = driver.findElement(By.xpath("//select[@id='months']"));
+
+        select=new Select(selectMonthElement);
+        select.selectByVisibleText("January");
+
+        //Select year
+        //driver.findElement(By.id("years"));
+        WebElement selectYearElement = driver.findElement(By.xpath("//select[@id='years']"));
+
+        select=new Select(selectYearElement);
+        select.selectByVisibleText("1989");
+
+
+        //click first checkbox
+
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")).click();
+
+        //click second checkbox
+
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")).click();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+
+    private void clickModule(String moduleName) {
+        String locator = "//a[contains(.,'" + moduleName + "')]";
+        driver.findElement(By.xpath(locator)).click();
+
+    }
+
+    private void signUp(){
         Faker faker = new Faker();
         String name = faker.name().firstName();
 
@@ -90,23 +173,6 @@ public class TC001 {
         emailElement.sendKeys(email);
 
         driver.findElement(By.xpath("//button[.='Signup']")).click();
-
-        WebElement label = driver.findElement(By.xpath("//h2[contains(.,'Enter Account Information')]"));
-
-        Assert.assertTrue(label.isDisplayed());
-
-
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.close();
-    }
-
-
-    private void clickModule(String moduleName) {
-        String locator = "//a[contains(.,'" + moduleName + "')]";
-        driver.findElement(By.xpath(locator)).click();
 
     }
 }
